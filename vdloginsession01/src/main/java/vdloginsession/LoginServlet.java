bf8d6ad8914c0fb2bad8ab798a3d90f0b1cf1385
phucnhannn <buiphucnhanbt@gmail.com>
2025-08-22 20:31:36 +0700

    sửa lỗi ví dụ login session

---
+++ vdloginsession01/src/main/java/vdloginsession/LoginServlet.java
@@ -0,0 +1,36 @@
+package vdloginsession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	 @Override
+	 protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, 
+	IOException {
+	 resp.setContentType("text/html");
+
+	 String username = req.getParameter("username");
+	 String password = req.getParameter("password");
+	 
+	 PrintWriter out = resp.getWriter();
+	 
+	 if (username.equals("trungnh") && password.equals("123")) {
+		 out.print("Chào mừng bạn, " + username);
+		 HttpSession session = req.getSession();
+		 session.setAttribute("name", username);
+		 } else {
+		 out.print("Tài khoản hoặc mật khẩu không chính xác");
+		 req.getRequestDispatcher("Login.html").include(req, 
+		resp);
+		 }
+
+	}
+}
